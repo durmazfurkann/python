28e2d2088ee79f22dc7f585bdb05be711b09e70f
yiğitcan <64696504+cakmak02273@users.noreply.github.com>
2022-11-15 20:24:08 +0300

    Update sunu5,1.py

--- sunu5,1.py
+++ sunu5,1.py
@@ -6,7 +6,7 @@
 # elif yas < 12:
 #     print("Sen Halit Değilsin")
 # elif yas > 80:
-#     print("Burda Ne İşin Var Moruk")    
+#     print("Burda Ne İşin Var Moruk")
 
 
 
@@ -28,16 +28,17 @@
 # if harf ==("a" or "e" or "i" or "u") :
 #     print("Girdiğiniz harf olan", harf, "sesli harfdir")
 # elif harf !=("a" or "e" or "i" or "u")  :
-#     print("Girdiğiniz harf olan", harf, "sessiz harfdir")    
+#     print("Girdiğiniz harf olan", harf, "sessiz harfdir")
 
 
 
 
-aylar=str(input("Bir Ay Giriniz"))
+aylar=input("Bir Ay Giriniz")
 
-if aylar ==(("Ocak" or "ocak") or "Mart" or "mart" or "Mayıs" or "mayıs" or "Temmuz" or "temmuz" or "Ağustos" or"ağustos" or "Ekim" or "ekim" or "Aralık" or "aralık"):        
+if aylar =="Ocak" or aylar == "ocak" or aylar == "Mart" or aylar == "mart" or aylar == "Mayıs" or aylar == "mayıs" or aylar == "Temmuz" or aylar == "temmuz" or aylar == "Ağustos" or aylar == "ağustos" or aylar == "Ekim" or aylar == "ekim" or aylar == "Aralık" or aylar == "aralık":
    print("31 gündür")
-elif aylar==("Şubat" and "şubat"):
+elif aylar=="Şubat" or aylar == "şubat":
     print("29 gündür")
 else:
-    print("30 gündür")    
+    print("30 gündür")
+
